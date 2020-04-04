1be7ea51f6339b0feffc0b6d489fa05adf51325e
Jakub Sałamun <zellanderplx@gmail.com>
2020-04-04 12:30:46 +0200

    ad

---
+++ ConsoleApp1/ConsoleApp1/CRUD.cs
@@ -0,0 +1,67 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+
+
+namespace ConsoleApp1
+
+{
+    class CRUD
+    {
+        public void Create(int id, string regionDescription, SqlConnection connection)
+        {
+            string sql = "INSERT INTO Northwind.dbo.Region(RegionID,RegionDescription) VALUES (@id,@regionName)";
+            var command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@regionName", regionDescription);
+            command.Parameters.AddWithValue("@id", id);
+
+            int param = command.ExecuteNonQuery();
+            if (param > 0)
+            {
+                Console.WriteLine("Dodano");
+            }
+        }
+        public void Read(SqlConnection connection)
+        {
+            SqlCommand command = new SqlCommand
+            {
+                CommandText = "SELECT * FROM Customers",
+                Connection = connection
+            };
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader["CompanyName"]);
+            }
+        }
+        public void Update(int id, string nowa_nazwa, SqlConnection connection)
+        {
+            string sql = "UPDATE Northwind.dbo.Region SET RegionDescription = @regionName WHERE RegionID=@id";
+            var command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@regionName", nowa_nazwa);
+            command.Parameters.AddWithValue("@id", id);
+            int param = command.ExecuteNonQuery();
+
+            if (param > 0)
+            {
+                Console.WriteLine("Zmieniono");
+            }
+        }
+
+        public void Delete(int id, SqlConnection connection)
+        {
+            string sql = "DELETE FROM Northwind.dbo.Region WHERE RegionID=@id";
+            var command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@id", id);
+            int param = command.ExecuteNonQuery();
+
+            if (param > 0)
+            {
+                Console.WriteLine("Usunieto");
+            }
+        }
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ConsoleApp1
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString =
+                @"Data Source=DESKTOP-MPTGS57\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            SqlConnection connection = sqlConnection;
+            connection.Open();
+
+
+            CRUD cRUD = new CRUD();
+            cRUD.Read(connection);
+            Console.WriteLine("-------------------------");
+            // cRUD.Create(13, "slask", connection);
+            //   cRUD.Update(13, "slask_2", connection);
+            //   cRUD.Delete(13, connection);
+            connection.Close();
+        }
+    }
+}
